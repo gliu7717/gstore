08da086501a9832efaea8d777eabf8f167698bb8
rayliu7717 <rayliu2004@gmail.com>
2025-12-03 19:05:41 -0500

    add header

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,3 @@
 export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || 'Prostore2'
 export const APP_DESCRIPTION=process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern ecommerce platform build with Next.js"
-export const SERVER_URL=process.env.NEXT_SERVER_URL || "http://localhost:3000"
+export const SERVER_URL=process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000"
