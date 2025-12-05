95dce8ca95dc1f6748663ce84db8776ce03d09a5
rayliu7717 <rayliu2004@gmail.com>
2025-12-04 19:14:45 -0500

    add sample products and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 //const delay = (ms) => new Promise((resolve) => setTimeout(resolve,ms));
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/header/product/product-list";
 
 const Homepage = () => {
   //await delay(2000)
-  return <>Prostore</>;
+  console.log(sampleData)
+
+  return <>
+  <ProductList data={sampleData.products} title='Newest Arrivals' limit={4}/>
+  </>;
 }
  
 export default Homepage;
