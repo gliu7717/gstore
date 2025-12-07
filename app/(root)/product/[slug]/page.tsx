f52b849e31c280a3ba53c7bc95ea7c0b1bc10347
rayliu7717 <rayliu2004@gmail.com>
2025-12-06 20:28:38 -0500

    prisma client fix

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const ProductDetailsPage = () => {
+    return ( <>details</> );
+}
+ 
+export default ProductDetailsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,6 +4,7 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "@/generated/prisma/client"
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 // Get latest product
 export async function getLatestProducts(){
     const connectionString = `${process.env.DATABASE_URL}`    
@@ -14,4 +15,11 @@ export async function getLatestProducts(){
         orderBy:{createAt:'desc'}
     })    
     return convertToPlainObject(data)
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug:string){
+    return await prisma.product.findFirst ({
+        where:{slug:slug}
+    })
 }
