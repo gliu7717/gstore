e2f0ab7a0401d45b3e9510911fc9c0300c649f9c
rayliu7717 <liu.geyang@gmail.com>
2025-12-17 15:26:31 -0500

    fixed compiling error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,14 +2,15 @@
 //import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/header/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
+import { Product } from "@/types";
 
 const Homepage = async () => {
   //await delay(2000)
-  const latestProducts = await getLatestProducts()
+  const latestProducts = await getLatestProducts() as unknown as Product[]
   console.log(latestProducts)
   return <>
-  <ProductList data={latestProducts} title='Newest Arrivals' limit={4}/>
+    <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
   </>;
 }
- 
+
 export default Homepage;
