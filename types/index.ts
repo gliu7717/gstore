35e178c1b5adc2e97daaec3ab895a4beec77c7e8
rayliu7717 <liu.geyang@gmail.com>
2025-12-30 10:33:16 -0500

    product u;date form

--- types/index.ts
+++ types/index.ts
@@ -4,7 +4,7 @@ import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressS
 export type Product = z.infer<typeof insertProductSchema> & {
     id: string;
     rating: number;
-    createAt: Date;
+    createdAt: Date;
 }
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
