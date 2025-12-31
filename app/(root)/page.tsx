f9ed7d9deae61ccac02e1eb56a44b77b28d6e1fb
rayliu7717 <liu.geyang@gmail.com>
2025-12-31 18:03:56 -0500

    featured product carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,21 @@
 //const delay = (ms) => new Promise((resolve) => setTimeout(resolve,ms));
 //import sampleData from "@/db/sample-data";
-import ProductList from "@/components/shared/header/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts, getFeaturedProducts } from "@/lib/actions/product.actions";
 import { Product } from "@/types";
+import ProductCarousel from '@/components/shared/product/product-carousel';
+
 
 const Homepage = async () => {
   //await delay(2000)
   const latestProducts = await getLatestProducts() as unknown as Product[]
+  const featuredProducts = await getFeaturedProducts() as unknown as Product[]
+
   console.log(latestProducts)
   return <>
+    {featuredProducts.length > 0 && (
+      <ProductCarousel data={featuredProducts} />
+    )}
     <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
   </>;
 }
