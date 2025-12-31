21f29c76633b464e7e5c9f9f2921efb7cb588072
rayliu7717 <liu.geyang@gmail.com>
2025-12-31 07:59:57 -0500

    category drawer

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,22 +2,24 @@ import Image from 'next/image'
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from './menu';
+import CategoryDrawer from './category-drawer';
 
 const Header = () => {
-    return ( <header className="w-full border-b">
+    return (<header className="w-full border-b">
         <div className="wrapper flex-between">
             <div className="flex-start">
-                <Link href='/' className='flex-start'>
-                <Image src='/images/logo.svg' alt = {`${APP_NAME} logo`} height= {48} width = {48} priority = {true} />
+                <CategoryDrawer />
+                <Link href='/' className='flex-start ml-4'>
+                    <Image src='/images/logo.svg' alt={`${APP_NAME} logo`} height={48} width={48} priority={true} />
 
-                <span className="hidden lg:block font-bold text-2xl ml-3">
-                    {APP_NAME}
-                </span>
+                    <span className="hidden lg:block font-bold text-2xl ml-3">
+                        {APP_NAME}
+                    </span>
                 </Link>
             </div>
-            <Menu/>
+            <Menu />
         </div>
-    </header> );
+    </header>);
 }
- 
+
 export default Header;
