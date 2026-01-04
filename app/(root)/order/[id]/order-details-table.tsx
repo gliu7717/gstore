3f4b332607d5fe45f608db0f1ea66b34ff062929
rayliu7717 <rayliu2004@gmail.com>
2026-01-04 17:35:15 -0500

    preview email in browser

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -44,7 +44,7 @@ const OrderDetailsTable = ({
 }) => {
   const {
     shippingAddress,
-    orderItems,
+    orderitems,
     itemsPrice,
     shippingPrice,
     taxPrice,
@@ -179,7 +179,7 @@ const OrderDetailsTable = ({
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {orderItems.map((item) => (
+                  {orderitems.map((item) => (
                     <TableRow key={item.slug}>
                       <TableCell>
                         <Link
